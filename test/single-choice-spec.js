f668b345334f5a39f58b07beed8def7bfc215428
ldyyukito <ldyyukito@outlook.com>
2015-08-05 18:04:03 +0800

    add single-choice

---
+++ test/single-choice-spec.js
@@ -0,0 +1,28 @@
+describe("scanner", function() {
+  describe("", function() {
+    var trueAnswer = [{
+      name: 'singleChoice1',
+      value: ['A'],
+      point: 2
+    }, {
+      name: 'multiple1',
+      value: ['A', 'B'],
+      point: 5
+    }, {
+      name: 'blank1',
+      value: ['统一建模语言'],
+      point: 2
+    }, {
+      name: 'judge1',
+      value: ['T'],
+      point: 1
+    }];
+
+    it("mark", function() {
+      var radioScore = new SingleChoice('singleChoice1', ['A']);
+      var result = radioScore.mark(trueAnswer);
+      expect(result).toEqual(2);
+    });
+
+  });
+});
---
+++ test/single-choice.js
@@ -0,0 +1,14 @@
+function SingleChoice(name, value) {
+  this.name = name;
+  this.value = value;
+}
+
+SingleChoice.prototype.mark = function(trueAnswer) {
+  var that = this;
+  var keyItem = trueAnswer.filter(function(val) {
+    return (that.name === val.name);
+  });
+  if (this.value[0] === keyItem[0].value[0]) {
+    return keyItem[0].point;
+  }
+};
